0921282280768c18c714df03a4950724e2418ff4
flu5h <sabziro1995@mail.ru>
2019-06-30 08:50:14 +0300

    card flip anim

--- app/src/main/java/com/example/pair_game/GridAdapter.java
+++ app/src/main/java/com/example/pair_game/GridAdapter.java
@@ -1,11 +1,16 @@
 package com.example.pair_game;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.os.Handler;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 
@@ -63,19 +68,36 @@ public class GridAdapter extends BaseAdapter {
 
             @Override
             public void onClick(View v) {
-                //if (clickFlag)
+                if (clickFlag)
                     if (card1 == null) {
-                        cards.forEach(card -> card.getImg().setImageResource(context.getResources()
-                                            .getIdentifier("shirt",
-                                                    "drawable",
-                                                    context.getPackageName())));
                         card1 = cards.get(position);
-                        card1.setPosition(position);
-                        card1.getImg().setImageResource(v.getContext()
-                                .getResources()
-                                .getIdentifier("img_" + cards.get(position).getRank() + "_of_" + cards.get(position).getSuit(),
+
+
+                        ObjectAnimator oa1 = ObjectAnimator.ofFloat(card1.getImg(), "scaleX", 1f, 0f);
+                        ObjectAnimator oa2 = ObjectAnimator.ofFloat(card1.getImg(), "scaleX", 0f, 1f);
+                        oa1.setInterpolator(new DecelerateInterpolator());
+                        oa2.setInterpolator(new AccelerateDecelerateInterpolator());
+                        card1.getImg().setImageResource(context.getResources()
+                                .getIdentifier("shirt",
                                         "drawable",
                                         context.getPackageName()));
+                        oa1.addListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                super.onAnimationEnd(animation);
+                                card1.setPosition(position);
+                                card1.getImg().setImageResource(v.getContext()
+                                        .getResources()
+                                        .getIdentifier("img_" + cards.get(position).getRank() + "_of_" + cards.get(position).getSuit(),
+                                                "drawable",
+                                                context.getPackageName()));
+                                oa2.start();
+                            }
+                        });
+                        oa1.setDuration(1000);
+                        oa2.setDuration(1000);
+                        oa1.start();
+
                         card1.getImg().refreshDrawableState();
                     } else if (card1 != cards.get(position)) {
                         clickFlag = false;
@@ -102,85 +124,27 @@ public class GridAdapter extends BaseAdapter {
                             card2.getImg().setVisibility(View.INVISIBLE);
                             card2.getBorder().setVisibility(View.INVISIBLE);
                             card2.getImg().setClickable(false);
+                            clickFlag = true;
+
                         } else {
-//                            handler.postDelayed(new Runnable() {
-//                                @Override
-//                                public void run() {
-//                                    clickFlag = true;
-//                                    card1.getImg().setImageResource(context.getResources()
-//                                            .getIdentifier("shirt",
-//                                                    "drawable",
-//                                                    context.getPackageName()));
-//                                    card2.getImg().setImageResource(context.getResources()
-//                                            .getIdentifier("shirt",
-//                                                    "drawable",
-//                                                    context.getPackageName()));
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+
                                     card1 = null;
-//                                }
-//                            }, 3000);
-//
+                                    cards.forEach(card -> card.getImg().setImageResource(context.getResources()
+                                            .getIdentifier("shirt",
+                                                    "drawable",
+                                                    context.getPackageName())));
+                                    clickFlag = true;
+                                }
+                            }, 3000);
+
                         }
                     }
             }
         });
-
-
-//            @Override
-//            public void onClick(View v) {
-//                if (card1 == null) {
-//                    card1 = cards.get(position);
-//                    //card1.getBorder().setCardBackgroundColor(Color.RED);
-//                    card1.getImg().setImageResource(context
-//                            .getResources()
-//                            .getIdentifier("img_" + cards.get(position).getRank() + "_of_" + cards.get(position).getSuit(),
-//                                    "drawable",
-//                                    context.getPackageName()));
-//                } else {
-//                    //card1.getBorder().setCardBackgroundColor(0);
-//                    card2 = cards.get(position);
-//                    card2.getImg().setImageResource(context
-//                            .getResources()
-//                            .getIdentifier("img_" + cards.get(position).getRank() + "_of_" + cards.get(position).getSuit(),
-//                                    "drawable",
-//                                    context.getPackageName()));
-//                    if (card1 != card2) {
-//                        if (card1.getRank() == card2.getRank())
-//                            if (((card1.getSuit() == "hearts") && (card2.getSuit() == "diamonds")) ||
-//                                    ((card1.getSuit() == "diamonds") && (card2.getSuit() == "hearts")) ||
-//                                    ((card1.getSuit() == "clubs") && (card2.getSuit() == "spades")) ||
-//                                    ((card1.getSuit() == "spades") && (card2.getSuit() == "clubs"))) {
-//                                card1.getImg().setVisibility(View.INVISIBLE);
-//                                card1.getBorder().setVisibility(View.INVISIBLE);
-//                                card1.getImg().setClickable(false);
-//                                card2.getImg().setVisibility(View.INVISIBLE);
-//                                card2.getBorder().setVisibility(View.INVISIBLE);
-//                                card2.getImg().setClickable(false);
-//                                card1.getBorder().setCardBackgroundColor(0);
-//                                card1 = null;
-//                                //imageView.setImageResource(0);
-//                            }
-//                    } else {
-//                        card1 = null;
-//                    }
-//                    card1=null;
-//                    cards.forEach(i -> i.getImg().setImageResource(context.getResources()
-//                            .getIdentifier("shirt",
-//                                    "drawable",
-//                                    context.getPackageName())));
-//                }
-//            }
-//        });
         return gridView;
     }
 
-    public Card openCard(int position) {
-        Card card;
-        card = cards.get(position);
-        card.getImg().setImageResource(context
-                .getResources()
-                .getIdentifier("img_" + cards.get(position).getRank() + "_of_" + cards.get(position).getSuit(),
-                        "drawable",
-                        context.getPackageName()));
-        return card;
-    }
 }
