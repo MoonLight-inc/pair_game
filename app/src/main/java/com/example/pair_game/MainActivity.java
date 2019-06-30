4fc34128353c945b0f45a72d19dcded4397a1b85
flu5h <sabziro1995@mail.ru>
2019-06-30 23:33:47 +0300

    -status bar

--- app/src/main/java/com/example/pair_game/MainActivity.java
+++ app/src/main/java/com/example/pair_game/MainActivity.java
@@ -1,27 +1,30 @@
 package com.example.pair_game;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-
-import android.graphics.Point;
-import android.net.Uri;
+import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Display;
+import android.util.TypedValue;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity   {
     //NavController navController;
+
+    public static int convertDpToPixels(float dp, Context context) {
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
+        return px;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //navController= Navigation.findNavController(this,R.id.fragment);
-
+        View decorView = getWindow().getDecorView();
+// Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
 
 
     }
-
-
 }
